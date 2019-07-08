a8b8d7fe95757814e382d145d4232ede2cf58d84
김형래 <besuc10bil@gmail.com>
2019-07-08 10:58:50 +0900

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <PHeader></PHeader>
+    <PFooter></PFooter>
+  </div>
+</template>
+
+<script>
+import PHeader from "./components/PHeader";
+import PFooter from "./components/PFooter";
+export default {
+  components: {
+    PHeader,
+    PFooter
+  }
+};
+</script>
+
+<style>
+</style>
