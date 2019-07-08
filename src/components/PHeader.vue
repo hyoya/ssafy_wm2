38f1ac871014ef9d527feefd30f98ca88cc8ea49
김형래 <besuc10bil@gmail.com>
2019-07-08 17:46:43 +0900

    test

--- src/components/PHeader.vue
+++ src/components/PHeader.vue
@@ -4,7 +4,9 @@
       <!-- toolbar start -->
       <v-toolbar fixed style="background:black;">
         <!-- logo -->
-        <v-btn icon to="/"><!--  TODO 여기는 로고 자리입니다. --> </v-btn>
+        <v-btn icon to="/">
+          <!--  TODO 여기는 로고 자리입니다. -->
+        </v-btn>
 
         <!-- title -->
         <v-toolbar-title class="white--text">Bobsleh</v-toolbar-title>
@@ -14,29 +16,25 @@
 
         <!-- sign in & sign up  -->
         <v-toolbar-items class="hidden-xs-only">
-            <v-btn
+          <v-btn
             v-for="item in menu"
             :key="item.icon"
             :to="item.link"
             flat
             class="white--text"
-            >{{ item.title }} </v-btn>
+          >{{ item.title }}</v-btn>
         </v-toolbar-items>
 
         <v-menu class="hidden-sm-and-up">
-          <v-toolbar-side-icon slot ="activator" class="white--text"></v-toolbar-side-icon>
+          <v-toolbar-side-icon slot="activator" class="white--text"></v-toolbar-side-icon>
           <v-list>
             <v-list-tile v-for="item in menu" :key="item.icon" :to="item.link">
               <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.title }}
-                </v-list-tile-title>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
           </v-list>
         </v-menu>
-
-
       </v-toolbar>
       <!-- toolbar end -->
     </v-flex>
@@ -45,15 +43,14 @@
 
 <script>
 export default {
-	name: 'PHeader',
-	props: {
-	},
-	data: () => ({
-    menu : [
-      {icon : 'signup', title:'Sign Up', link:"/signup"},
-      {icon : 'signin', title:'Sign In', link:"/signin"},
-    ],
-  }),
-
-}
+  name: "PHeader",
+  props: {},
+  data: () => ({
+    menu: [
+      { icon: "signup", title: "Sign Up", link: "/signup" },
+      { icon: "signin", title: "Sign In", link: "/signin" },
+      { icon: "userInfo", title: "UserInfo", link: "/userPage" }
+    ]
+  })
+};
 </script>
