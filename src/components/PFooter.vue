62e92e7164b719ea33ddb5a37d9b5d700021dd84
Park Sangho <sxngho@naver.com>
2019-07-08 13:33:42 +0900

    Req. 2-5-1 :: 날씨 정보를 제공하는 API를 찾아서 Footer에 연결합니다.

--- src/components/PFooter.vue
+++ src/components/PFooter.vue
@@ -1,11 +1,50 @@
 <template>
-  <div>footer</div>
-  commit Test
+  <div id="app">
+    <div>
+      <h1> ---------------------------------------------------------------------------------------------------------------------------------------------- </h1>
+      <h2> 오늘의 날씨 </h2>
+      <p>도시명 : {{ city }}</p>
+      <p>현재 습도 : {{ humidity }}% / 100% </p>
+      <p> 날씨 : {{  weather[0].main }} </p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+
+export default {
+  name: 'pFooter',
+  data() {
+    return {
+      city: '',
+      humidity: '',
+      weather: '',
+    }
+  },
+  computed: {
+      hasResult: function() {
+        return this.posts.length > 0
+      }
+  },
+  mounted() {
+      const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q=Daejeon&appid=8524498325d50c1573d0f08755ed342c'
+      this.$http.get(`${BASE_URL}`)
+      .then((result) => {
+        this.city = result.data.name
+        this.humidity = result.data.main.humidity
+        this.weather = result.data.weather
+      })
+  }
+}
 </script>
 
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
