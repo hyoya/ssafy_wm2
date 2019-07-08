3395785e9e4f2ea4028c2ecbfceb9c0cc97b479a
hyoya <ghtybndss@gmail.com>
2019-07-08 19:48:40 +0900

    Fix :: Sign In Modal

--- src/components/PHeader.vue
+++ src/components/PHeader.vue
@@ -13,28 +13,11 @@
         <v-spacer></v-spacer>
 
         <!-- sign in & sign up  -->
-        <v-toolbar-items class="hidden-xs-only">
-            <v-btn
-            v-for="item in menu"
-            :key="item.icon"
-            :to="item.link"
-            flat
-            class="white--text"
-            >{{ item.title }} </v-btn>
+        <v-toolbar-items >
+          <LoginModal/>
+          <v-btn flat class="white--text">Sign Up</v-btn>
         </v-toolbar-items>
 
-        <v-menu class="hidden-sm-and-up">
-          <v-toolbar-side-icon slot ="activator" class="white--text"></v-toolbar-side-icon>
-          <v-list>
-            <v-list-tile v-for="item in menu" :key="item.icon" :to="item.link">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.title }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-menu>
 
 
       </v-toolbar>
@@ -44,10 +27,15 @@
 </template>
 
 <script>
+import LoginModal from "./LoginModal"
+
 export default {
 	name: 'PHeader',
 	props: {
 	},
+  components :{
+    LoginModal,
+  },
 	data: () => ({
     menu : [
       {icon : 'signup', title:'Sign Up', link:"/signup"},
