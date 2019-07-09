1f425b9009f4911f1c8ad3d238704c5982ab0399
김형래 <besuc10bil@gmail.com>
2019-07-09 14:23:36 +0900

    commit test

--- src/components/PHeader.vue
+++ src/components/PHeader.vue
@@ -15,13 +15,26 @@
         <v-spacer></v-spacer>
 
         <!-- sign in & sign up  -->
-        <v-toolbar-items >
-          <LoginModal/>
-          <v-btn flat class="white--text">Sign Up</v-btn>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn
+            v-for="item in menu"
+            :key="item.icon"
+            :to="item.link"
+            flat
+            class="white--text"
+          >{{ item.title }}</v-btn>
         </v-toolbar-items>
 
-
-
+        <v-menu class="hidden-sm-and-up">
+          <v-toolbar-side-icon slot="activator" class="white--text"></v-toolbar-side-icon>
+          <v-list>
+            <v-list-tile v-for="item in menu" :key="item.icon" :to="item.link">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
       </v-toolbar>
       <!-- toolbar end -->
     </v-flex>
@@ -29,21 +42,15 @@
 </template>
 
 <script>
-import LoginModal from "./LoginModal"
-
 export default {
-	name: 'PHeader',
-	props: {
-	},
-  components :{
-    LoginModal,
-  },
-	data: () => ({
-    menu : [
-      {icon : 'signup', title:'Sign Up', link:"/signup"},
-      {icon : 'signin', title:'Sign In', link:"/signin"},
-    ],
-  }),
-
-}
+  name: "PHeader",
+  props: {},
+  data: () => ({
+    menu: [
+      { icon: "signup", title: "Sign Up", link: "/signup" },
+      { icon: "signin", title: "Sign In", link: "/signin" },
+      { icon: "userInfo", title: "User Info", link: "/userInfo" }
+    ]
+  })
+};
 </script>
--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,56 +1,52 @@
 <template>
-
   <v-layout column>
     <v-flex>
       <!-- -->
-      <div style='height:300px;'>아직 미정입니다.</div>
+      <div style="height:300px;">아직 미정입니다.</div>
       <div>
-        <h1>회원가입하자</h1>
-        Email : <input v-model='id'><br>
-        Password : <input v-model='password' type="password">
-        <button @click='check(id, password)'>회원가입하기</button>
+        <h1>회원가입하자</h1>Email :
+        <input v-model="id" />
+        <br />Password :
+        <input v-model="password" type="password" />
+        <button @click="check(id, password)">회원가입하기</button>
       </div>
     </v-flex>
   </v-layout>
-
 </template>
 
 <script>
-import FirebaseService from "@/services/FirebaseService";
+// import FirebaseService from "@/services/FirebaseService";
 // const auth = FirebaseService.auth
 
 export default {
-	name: 'SignUpForm',
-	props: {
-	},
-	data() {
-		return {
-      id : '',
-      password : ''
-		}
+  name: "SignUpForm",
+  props: {},
+  data() {
+    return {
+      id: "",
+      password: ""
+    };
   },
   methods: {
-    check : function() {
-      FirebaseService.auth().createUserWithEmailAndPassword(this.id, this.password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode)
-      console.log(errorMessage)
-      // ...
-    });
-    }
+    // check: function() {
+    //   FirebaseService.auth()
+    //     .createUserWithEmailAndPassword(this.id, this.password)
+    //     .catch(function(error) {
+    //       var errorCode = error.code;
+    //       var errorMessage = error.message;
+    //       c
+    //     });
+    // }
   }
-
-}
+};
 </script>
 <style>
 input {
-  border : 1px solid;
-  margin : 10px;
+  border: 1px solid;
+  margin: 10px;
 }
 button {
-  border : 1px solid red;
-  color : red;
+  border: 1px solid red;
+  color: red;
 }
 </style>
