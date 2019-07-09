81a0ba1045dc761a46e6d4ad80e7cf50d4402204
Kim-Seul-Gi <rlatmf2147@naver.com>
2019-07-09 13:31:06 +0900

    firebase.js 통합중

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import store from './store'
 import './registerServiceWorker'
 
 import axios from 'axios'
-import * as firebase from "firebase/app";
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
@@ -31,14 +30,3 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app')
-
-const firebase_config = {
-  apiKey: 'AIzaSyDu-5lDS7UuTjeSz2lcYLrJmst_amq3AA8',
-  authDomain: 'vue-reboot.firebaseapp.com',
-  databaseURL: 'https://vue-reboot.firebaseio.com',
-  projectId: 'vue-reboot',
-  storageBucket: '',
-  messagingSenderId: '395343379837',
-  appId: '1:395343379837:web:62eb22d57ca1520e'
-};
-firebase.initializeApp(firebase_config);
