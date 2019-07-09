58b63e9685d6e424a7907b7cbd0e8446a967dd48
김슬기 <rlatmf2147@naver.com>
2019-07-09 00:24:20 +0000

    Revert "Merge branch 'palkim' into 'master'"
    This reverts merge request !15

--- src/router.js
+++ src/router.js
@@ -1,33 +1,26 @@
 // install npm
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
 
 // vue page
-import MainPage from "./views/MainPage.vue";
-import UserPage from "./views/UserPage.vue";
-import SignUpPage from './views/SignUpPage.vue';
+import MainPage from './views/MainPage.vue'
+import SignUpPage from './views/SignUpPage.vue'
 
-
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+		{
+			path: '/',
+			name: 'main',
+			component: MainPage
+		},
     {
-      path: "/",
-      name: "main",
-      component: MainPage
-    },
-    {
-      path: "/userPage",
-      name: "userPage",
-      component: UserPage
-    },
-    {
-	  path: '/signup',
-	  name: 'signup',
-	  component: SignUpPage
-	}
+			path: '/signup',
+			name: 'signup',
+			component: SignUpPage
+		}
   ]
-});
+})
